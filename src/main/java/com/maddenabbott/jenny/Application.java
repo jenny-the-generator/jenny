52e00a89df118c93fadc8c1436c068bad6b95bf5
Rupert Madden-Abbott <Rupert.Madden-Abbott@bristol.ac.uk>
2015-04-26 10:15:46 +0100

    Construct command groups and pass to command help command.

--- src/main/java/com/maddenabbott/jenny/Application.java
+++ src/main/java/com/maddenabbott/jenny/Application.java
@@ -1,7 +1,15 @@
 package com.maddenabbott.jenny;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import static java.util.Arrays.asList;
+
+import com.maddenabbott.jenny.cli.Name;
+import com.maddenabbott.jenny.cli.SubCommands;
 import com.maddenabbott.jenny.command.AddCommand;
 import com.maddenabbott.jenny.command.Command;
 import com.maddenabbott.jenny.command.CommandException;
@@ -9,6 +17,7 @@ import com.maddenabbott.jenny.command.ListCommand;
 import com.maddenabbott.jenny.command.NewCommand;
 import com.maddenabbott.jenny.command.RemoveCommand;
 import com.maddenabbott.jenny.command.RenameCommand;
+import com.maddenabbott.jenny.command.help.CommandHelpCommand;
 import com.maddenabbott.jenny.command.help.HelpCommand;
 
 /**
@@ -43,8 +52,24 @@ public class Application {
     List<Class<? extends Command>> commands = asList(HelpCommand.class, AddCommand.class,
         RemoveCommand.class, RenameCommand.class, ListCommand.class, NewCommand.class);
 
+    Map<String, List<Class<? extends Command>>> commandGroups = new HashMap<>();
+
+    for (Class<? extends Command> command : commands) {
+      Name name = command.getAnnotation(Name.class);
+      SubCommands subCommands = command.getAnnotation(SubCommands.class);
+      if (name != null) {
+        List<Class<? extends Command>> commandGroup = new ArrayList<>();
+        commandGroup.add(command);
+        if (subCommands != null) {
+          commandGroup.addAll(asList(subCommands.value()));
+        }
+        commandGroups.put(name.value(), commandGroup);
+      }
+    }
+
     switch (parameters.length) {
       case 0: return new HelpCommand(commands);
+      case 1: return new CommandHelpCommand(commandGroups, parameters[0]);
       default: throw new CommandException("Incorrect number of parameters");
     }
   }
