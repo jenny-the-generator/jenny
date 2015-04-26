f20f1a2672df20abc24b6c1b2788e9afc65edf7d
Rupert Madden-Abbott <Rupert.Madden-Abbott@bristol.ac.uk>
2015-04-26 09:36:14 +0100

    Add method to extract command name from command line args.

--- src/main/java/com/maddenabbott/jenny/Application.java
+++ src/main/java/com/maddenabbott/jenny/Application.java
@@ -4,6 +4,17 @@ package com.maddenabbott.jenny;
  * The main entry point for Jenny.
  */
 public class Application {
+  private static final String defaultCommand = "help";
+
   public static void main(String[] args) {
+    String commandName = getCommandName(args, defaultCommand);
+  }
+
+  private static String getCommandName(final String[] args, final String defaultCommandName) {
+    if (args.length > 0 && args[0] != null) {
+      return args[0];
+    } else {
+      return defaultCommandName;
+    }
   }
   }
