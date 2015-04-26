ea168e576e4b3f62a4161c2318d911337f58a921
Rupert Madden-Abbott <Rupert.Madden-Abbott@bristol.ac.uk>
2015-04-26 10:14:32 +0100

    Specify parameters needed for command help command.

--- src/main/java/com/maddenabbott/jenny/command/help/CommandHelpCommand.java
+++ src/main/java/com/maddenabbott/jenny/command/help/CommandHelpCommand.java
@@ -1,9 +1,25 @@
 package com.maddenabbott.jenny.command.help;
 
+import java.util.List;
+import java.util.Map;
+
+import com.maddenabbott.jenny.cli.Parameter;
 import com.maddenabbott.jenny.cli.Summary;
 import com.maddenabbott.jenny.command.Command;
+
 @Summary("Pass the name of a command for help with that command.")
 public class CommandHelpCommand implements Command {
+  @Parameter("The name of the command you would like more help with.")
+  private final String command;
+  private final Map<String, List<Class<? extends Command>>> commandGroups;
+
+  public CommandHelpCommand(
+      final Map<String, List<Class<? extends Command>>> commandGroups,
+      final String command) {
+    this.command = command;
+    this.commandGroups = commandGroups;
+  }
+
   @Override
   public void run() {
   }
