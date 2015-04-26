9a099a5c5f33ae8df8e5881e1d2d5f9862dcadb3
Rupert Madden-Abbott <Rupert.Madden-Abbott@bristol.ac.uk>
2015-04-26 10:07:34 +0100

    Add skeleton for command help command.

---
+++ src/main/java/com/maddenabbott/jenny/command/help/CommandHelpCommand.java
@@ -0,0 +1,10 @@
+package com.maddenabbott.jenny.command.help;
+
+import com.maddenabbott.jenny.cli.Summary;
+import com.maddenabbott.jenny.command.Command;
+@Summary("Pass the name of a command for help with that command.")
+public class CommandHelpCommand implements Command {
+  @Override
+  public void run() {
+  }
+  }
