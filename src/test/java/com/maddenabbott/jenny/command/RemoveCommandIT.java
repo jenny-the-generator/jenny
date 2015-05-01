5a02cca38d399dfb421d93333cf2b74fe5d07ebd
Rupert Madden-Abbott <Rupert.Madden-Abbott@bristol.ac.uk>
2015-05-01 16:48:05 +0100

    Add test for remove command.

---
+++ src/test/java/com/maddenabbott/jenny/command/RemoveCommandIT.java
@@ -0,0 +1,32 @@
+package com.maddenabbott.jenny.command;
+
+import com.maddenabbott.jenny.Application;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import static com.maddenabbott.jenny.test.OutputMatcher.isIn;
+import static com.maddenabbott.jenny.test.RepositoryUtil.addRepo;
+import static com.maddenabbott.jenny.test.RepositoryUtil.removeRepos;
+import static org.junit.Assert.assertThat;
+
+public class RemoveCommandIT {
+  @Rule
+  public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+  @After
+  public void tearDown() throws Exception {
+    removeRepos();
+  }
+
+  @Test
+  public void shouldRemoveRepository() throws Exception {
+    addRepo("test", "test-template-repo-1");
+    
+    Application.main(new String[] { "remove", "test" });
+    
+    assertThat(log.getLog(), isIn("remove/remove"));
+  }
+  
+  
+}
